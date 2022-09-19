3a2b8b1f7c2d4e620285f2298f03a7e0034a27c4
Nima Zare <NimaDeveloper@yahoo.com>
2022-09-19 10:58:45 +0430

    Delete BlazorUploadFile/Shared directory

--- BlazorUploadFile/Shared/UploadResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorUploadFile.Shared
-{
-    public class UploadResult
-    {
-        public String? FileName { get; set; }
-        public String? StoredFileName { get; set; }
-    }
-}
